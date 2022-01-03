c70aae57a27a22dc52adf93c00a948251d5b3e72
Vanderson Tavares <vandersontavares32@gmail.com>
2022-01-03 12:09:36 -0300

    Exceptions, validation and security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.devsuperior.dslearnbds.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dslearnbds.services.exceptions.DatabaseException;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
+
+//intercepta exceções no resource
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class) //sempre que houver alguma exceção nos controladores rest, o tratamento vais ser direcionado pra cá
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		HttpStatus status = HttpStatus.NOT_FOUND; //retorna error 404
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value()); //o .value converte para inteiro
+		err.setError("Resource not Found"); //esse erro irá aparecer quando buscarmos a entidade
+		err.setMessage(e.getMessage()); //pega a msnsagem definida la no CategoryService
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class) //sempre que houver alguma exceção nos controladores rest, o tratamento vais ser direcionado pra cá
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		HttpStatus status = HttpStatus.BAD_REQUEST; //retorna error 400
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value()); //o .value converte para inteiro
+		err.setError("Database Exception"); 
+		err.setMessage(e.getMessage()); //pega a msnsagem definida la no CategoryService
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) //sempre que houver alguma exceção nos controladores rest, o tratamento vais ser direcionado pra cá
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //retorna error 422
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value()); //o .value converte para inteiro
+		err.setError("Validation Exception"); 
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for(FieldError f: e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
